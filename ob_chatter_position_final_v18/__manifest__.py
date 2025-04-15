becf9469ffa1fc20a21fe11ca08e444d023e4edd
cudio-tech <139472987+cudio-tech@users.noreply.github.com>
2025-04-15 11:43:36 -0400

    add v18 upgrade chatter test

---
+++ ob_chatter_position_final_v18/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Configurable Chatter Position',
+    'summary': 'Allows users to configure chatter position on forms',
+    'version': '1.1.0',
+    'category': 'Productivity',
+    'author': 'Odoobeing',
+    'depends': ['base', 'web'],
+    'data': [
+        'views/res_users.xml',
+        'views/web.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'ob_chatter_position/static/src/views/form/form_buttons.xml',
+            'ob_chatter_position/static/src/views/form/form_chatter_position.js',
+            'ob_chatter_position/static/src/scss/chatter_position.scss',
+        ],
+    },
+    'license': 'LGPL-3',
+    'installable': True,
+    'application': False,
+}
